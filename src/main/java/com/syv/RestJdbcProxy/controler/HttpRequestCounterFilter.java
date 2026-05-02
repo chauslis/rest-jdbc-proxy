8114187f7d9ac8d89724e83edf5becb7a6b5a5bf
chauslis <ystadnik@gmail.com>
2026-05-01 23:26:14 -0400

    Add HTTP request counter filter

---
+++ src/main/java/com/syv/RestJdbcProxy/controler/HttpRequestCounterFilter.java
@@ -0,0 +1,63 @@
+package com.syv.RestJdbcProxy.controler;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class HttpRequestCounterFilter extends OncePerRequestFilter {
+
+    private final MeterRegistry meterRegistry;
+
+    public HttpRequestCounterFilter(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+        String url = firstTwoSegmentsFast(request.getRequestURI());
+        Tags tags = Tags.of(
+                "method", request.getMethod(),              // GET / POST
+                "uri", url, // bounded
+                "status", String.valueOf(response.getStatus())
+        );
+
+        meterRegistry.counter("http.requests."+ url +".count", tags).increment();
+    }
+
+    private String firstTwoSegmentsFast(String uri) {
+        if (uri == null || uri.isBlank()) {
+            return "unknown";
+        }
+
+        String trimmed = uri.startsWith("/") ? uri.substring(1) : uri;
+
+        int firstSlash = trimmed.indexOf('/');
+        if (firstSlash < 0) {
+            // only one segment
+            return trimmed;
+        }
+
+        int secondSlash = trimmed.indexOf('/', firstSlash + 1);
+        if (secondSlash < 0) {
+            // exactly two segments
+            return trimmed.replace('/','.');
+        }
+
+        // more than two segments
+        return trimmed.substring(0, secondSlash).replace('/','.');
+    }
+}
